506b8c46f2c3dc46d65322c9974fe201ee44e0f8
Afamawal <muhdafham8@gmail.com>
2025-03-27 17:41:21 +0800

    Add stringbuffer and stringbuilder examples

--- src/string/Main.java
+++ src/string/Main.java
@@ -1,5 +1,12 @@
 package string;
 
+/**
+ * References
+ * 1. https://www.youtube.com/watch?v=1Vvgk_UAC0A
+ * 2. https://www.youtube.com/watch?v=bbjitAm5xB0
+ * 3. https://muratakkan.medium.com/comparing-string-stringbuffer-and-stringbuilder-in-java-128a08ff2a5f
+ * 4.
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +17,11 @@ public class Main {
         System.out.println(obj == obj1);
         System.out.println(obj1 == obj2);
         System.out.println(obj);
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(" World");
+        System.out.println(stringBuffer);
+        System.out.println(stringBuffer.length());
     }
 
 
